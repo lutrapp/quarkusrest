8942c4a6ab1fd13d6e7c6052aef9700b86d4fca8
lutrapp <lutrapp@gmail.com>
2022-06-12 23:26:57 -0300

    ajuste

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
 INSERT INTO public.users
 (id, age, "name")
-VALUES(1, 17, 'Gabriela Trapp');
+VALUES(1, 17, 'Fulana');
 INSERT INTO public.users
 (id, age, "name")
-VALUES(2, 10, 'Hercules Trapp');
+VALUES(2, 10, 'Beltrano');
 INSERT INTO public.users
 (id, age, "name")
-VALUES(3, 14, 'Bernardo Cesar Trapp');
+VALUES(3, 14, 'Pafuncio');
 INSERT INTO public.users
 (id, age, "name")
-VALUES(4, 45, 'Emiliano Cesar Trapp');
+VALUES(4, 45, 'Pessoa');
 INSERT INTO public.users
 (id, age, "name")
-VALUES(5, 40, 'Luciane Pohlod Trapp');
+VALUES(5, 40, 'Nova Pessoa');
